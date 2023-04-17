add33036b40d51e257a083362d9130e0386579c5
luismda <luismigueldutraalves@gmail.com>
2023-04-17 08:41:33 -0300

    feat: create error global handler

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,26 @@
 import fastify from 'fastify'
+import { ZodError } from 'zod'
 
 import { organizationsRoutes } from './http/controllers/organizations/routes'
+import { env } from './env'
 
 export const app = fastify()
 
 app.register(organizationsRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation error.',
+      issues: error.format(),
+    })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // Log error to external service (DataDog, NewRelic, Sentry)
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
